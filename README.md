db329c61ecbd278afc320b3fbf9df2a16e797e7f
Jason Ray <jason.ray@agilex.com>
2014-04-14 17:13:44 -0400

    add abilty to specify duration on a pause

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ Install node dependencies (including formidable)
 ```
 npm install 
 ```
+
+Usage
+-----
+
+Pause
+=====
+The `pause` service waits for a specified amount of time, then returns an `HTTP 200`.  Useful for performance testing to see the effect of having lots of calls that seem to take a period of time to return.
+
+```
+curl -i http://127.0.0.1:8888/pause?duration=3000
+```
--- requestHandlers/pauseService.js
+++ requestHandlers/pauseService.js
@@ -1,10 +1,18 @@
 function pause(request, response, next) {
 	console.log("Request handler 'pause' was called.");
 
+	var duration = parseInt(request.query.duration, 10);
+	if (isNaN(duration)) {
+		console.log('duration not specified, defaulting to 10 seconds');
+		duration = 10*1000;
+	} else {
+		console.log('duration specified to %s', duration);
+	}
+
 	setTimeout(function() {
 		console.log("pause timer fired");
 		response.end();
-	}, 1000*60*60);
+	}, duration);
 
 	next();
 }
