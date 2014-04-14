fa841722c634e55378cc7f8349b4fbce7c218c9d
Jason Ray <jason.ray@agilex.com>
2014-04-14 16:43:41 -0400

    run perf tests against pause service

--- requestHandlers/pauseService.js
+++ requestHandlers/pauseService.js
@@ -1,10 +1,14 @@
 function pause(response, request) {
 	console.log("Request handler 'pause' was called.");
 
+	var url = require('url');
+	var url_parts = url.parse(request.url, true);
+	var query = url_parts.query;
+
 	setTimeout(function() {
 		console.log("pause timer fired");
 		response.end();
-	}, 5000);
+	}, 1000*60*60);
 
 }
 
