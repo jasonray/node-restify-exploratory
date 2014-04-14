de308e7ab6120b668206b76519a5604fa1a4207f
Jason Ray <jason.ray@agilex.com>
2014-04-14 19:13:07 -0400

    simplified hello world resource

--- requestHandlers/helloworld.js
+++ requestHandlers/helloworld.js
@@ -1,38 +1,34 @@
 var Negotiator = require('negotiator');
 
-function sayHello(response, request) {
+function sayHello(request, response, next) {
 	console.log("Request handler 'hello' was called.");
 
 	var availableMediaTypes = ['text/text', 'text/plain', 'application/json', 'application/xml', 'text/html'];
 	var negotiator = new Negotiator(request);
 	var negotiatedMediaType = negotiator.mediaType(availableMediaTypes);
 
+	var sampleQueryParam = request.query.q;
+	var sampleHeaderParam = request.header('h');
+	var sampleHeaderParamWithDefault = request.header('h2', 'default');
+
 	if (negotiatedMediaType == 'text/text' || negotiatedMediaType == 'text/plain') {
-		response.writeHead(200, {
-			"Content-Type": negotiatedMediaType
-		});
-		response.write('hello world');
-		response.end();
+		response.send('hello world');
 	} else if (negotiatedMediaType == 'application/json') {
-		response.writeHead(200, {
-			"Content-Type": "application/json"
-		});
-		response.write('{message: "hello world}');
-		response.end();
+		responseMessage = {
+			message: 'hello world',
+			h: sampleHeaderParam,
+			h2: sampleHeaderParamWithDefault,
+			q: sampleQueryParam
+		};
+
+		response.json(responseMessage);
 	} else if (negotiatedMediaType == 'text/html') {
-		response.writeHead(200, {
-			"Content-Type": "text/html"
-		});
-		response.write('<html><body>hello world</body></html>');
-		response.end();
+		response.send('<html><body>hello world</body></html>');
 	} else {
-		response.writeHead(200, {
-			"Content-Type": "text/text"
-		});
-		response.write('hello world');
-		response.end();
+		response.send('hello world');
 	}
 
+	next();
 }
 
 exports.sayHello = sayHello;
