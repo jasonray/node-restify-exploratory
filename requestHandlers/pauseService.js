b66f30407ee78840467ad9c6be893742767dc8a0
Jason Ray <jason.ray@agilex.com>
2014-04-15 07:42:58 -0400

    formatting

--- requestHandlers/pauseService.js
+++ requestHandlers/pauseService.js
@@ -1,12 +1,14 @@
 function pause(request, response, next) {
-	var logger = require('bunyan').createLogger({name: "pauseService"});
+	var logger = require('bunyan').createLogger({
+		name: "pauseService"
+	});
 
 	logger.info("Request handler 'pause' was called.");
 
 	var duration = parseInt(request.query.duration, 10);
 	if (isNaN(duration)) {
 		logger.info('duration not specified, defaulting to 10 seconds');
-		duration = 10*1000;
+		duration = 10 * 1000;
 	} else {
 		logger.info('duration specified to %s', duration);
 	}
