517c8fb319ca0e50c643fb50bb387d245a09aaf0
Jason Ray <jason.ray@agilex.com>
2014-04-15 07:42:52 -0400

    formatting

--- requestHandlers/math.js
+++ requestHandlers/math.js
@@ -1,21 +1,25 @@
 function add(req, res, next) {
-	var logger = require('bunyan').createLogger({name: "mathService"});
+	var logger = require('bunyan').createLogger({
+		name: "mathService"
+	});
 
 	var x = parseInt(req.query.x, 10);
 	var y = parseInt(req.query.y, 10);
 	var sum = x + y;
-	logger.info('%s + %s = %s',x ,y, sum);
+	logger.info('%s + %s = %s', x, y, sum);
 	res.send(sum.toString());
 	next();
 }
 
 function subtract(req, res, next) {
-	var logger = require('bunyan').createLogger({name: "mathService"});
+	var logger = require('bunyan').createLogger({
+		name: "mathService"
+	});
 
 	var x = parseInt(req.query.x, 10);
 	var y = parseInt(req.query.y, 10);
 	var diff = x - y;
-	logger.info('%s - %s = %s',x ,y, diff);
+	logger.info('%s - %s = %s', x, y, diff);
 	res.send(diff.toString());
 	next();
 }
