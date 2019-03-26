186a0ba9e9885509c695af5d412697b04798de3c
jason.ray <jason.ray@ablevets.com>
2019-03-26 10:02:09 -0400

    clean up dep and vulnerabilities

--- app.js
+++ app.js
@@ -9,15 +9,9 @@ server.use(restify.requestLogger({
 
 var activeCalls = 0;
 server.use(function(req, res, next) {
-	var logger = require('bunyan').createLogger({
-		name: "activeCallsFilter"
-	});
-
 	activeCalls++;
-	logger.info('active calls (pre): ' + activeCalls);
 	next();
 	activeCalls--;
-	logger.info('active calls: (post): ' + activeCalls);
 });
 
 server.get('/hello', require("./requestHandlers/helloworld").sayHello);
@@ -27,18 +21,8 @@ server.get('/math/add', require("./requestHandlers/math").add);
 server.get('/math/subtract', require("./requestHandlers/math").subtract);
 
 server.use(function handler2(req, res, next) {
-	var logger = require('bunyan').createLogger({
-		name: "handler2"
-	});
-
-	logger.info('handler2');
 	next();
 });
 
 server.listen(8888, function() {
-	var logger = require('bunyan').createLogger({
-		name: "server"
-	});
-
-	logger.info('%s listening at %s', server.name, server.url);
 });
